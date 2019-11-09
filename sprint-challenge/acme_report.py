f604c2555e0ac3aceefef80dc625a27cadf59709
sapinspys <garri009@fiu.edu>
2019-11-08 18:57:20 -0800

    Added first method to acme report

--- sprint-challenge/acme_report.py
+++ sprint-challenge/acme_report.py
@@ -1,4 +1,13 @@
 from random import randint
 
-['Awesome', 'Shiny', 'Impressive', 'Portable', 'Improved']
-['Anvil', 'Catapult' 'Disguise' 'Mousetrap', '???']
+def generate_products(count=30):
+    adj = ['Awesome', 'Shiny', 'Impressive', 'Portable', 'Improved']
+    noun = ['Anvil', 'Catapult' 'Disguise' 'Mousetrap', '???']
+    product_list = []
+
+    while count > 0:
+        product = f'{adj[randint(0,len(adj))]} {noun[randint(0,len(noun))]}
+        product_list.append(product)
+        count --
+    
+    return product_list
