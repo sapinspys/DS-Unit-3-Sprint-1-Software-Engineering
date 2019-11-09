f25006724bd1d695e6871509ab2ce3d712f51cf8
sapinspys <garri009@fiu.edu>
2019-11-08 18:44:20 -0800

    Added new child class to acme script

--- sprint-challenge/acme.py
+++ sprint-challenge/acme.py
@@ -1,18 +1,19 @@
 from random import randint
 
+
 class Product:
-    def __init__(self, name, price=10, weight=20, flammability=0.5, id):
-        self.name = name    
+    def __init__(self, name, price=10, weight=20, flammability=0.5):
+        self.name = name
         self.price = price
         self.weight = weight
         self.flammability = flammability
-        self.id = randint(1000000,9999999)
+        self.id = randint(1000000, 9999999)
 
     def stealability(self):
         ratio = self.price/self.weight
         if ratio < 0.5:
             return 'Not so stealable..'
-        elif 0.5 < ratio < 1:
+        elif ratio < 1:
             return 'Kinda stealable'
         else:
             return 'Very stealable!'
@@ -21,7 +22,23 @@ class Product:
         product = self.flammability*self.weight
         if product < 10:
             return '...fizzle'
-        elif 10 < product < 50:
+        elif product < 50:
             return '...boom!'
         else:
             return '...BABOOM!!'
+
+
+class BoxingGlove(Product):
+    def __init__(self, name, price=10, weight=10, flammability=0.5):
+        super().__init__(name, price, weight, flammability)
+
+    def explode(self):
+        return "...it's a glove."
+
+    def punch(self):
+        if self.weight < 5:
+            return 'That tickles.'
+        elif self.weight < 15:
+            return 'Hey that hurt!'
+        else:
+            return 'OUCH!'
