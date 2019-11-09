ebf20c246736510a914bcc5811adea191eedd6b4
sapinspys <garri009@fiu.edu>
2019-11-08 19:16:30 -0800

    Added inventory report method to acme report script

--- sprint-challenge/acme_report.py
+++ sprint-challenge/acme_report.py
@@ -1,13 +1,31 @@
-from random import randint
+from random import randint, uniform
+from statistics import mean
+from acme import Product
+
+adj = ['Awesome', 'Shiny', 'Impressive', 'Portable', 'Improved']
+noun = ['Anvil', 'Catapult' 'Disguise' 'Mousetrap', '???']
 
 def generate_products(count=30):
-    adj = ['Awesome', 'Shiny', 'Impressive', 'Portable', 'Improved']
-    noun = ['Anvil', 'Catapult' 'Disguise' 'Mousetrap', '???']
+    """Takes in a number and generates a list of that number of products randomly generated from existing adjective and noun lists."""
     product_list = []
 
-    while count > 0:
-        product = f'{adj[randint(0,len(adj))]} {noun[randint(0,len(noun))]}
-        product_list.append(product)
-        count --
+    for _ in range(count):
+        name =  f'{adj[randint(0,len(adj))]} {noun[randint(0,len(noun))]}'
+        price = randint(5,100)
+        weight = randint(5, 100)
+        flammability = uniform(0, 2.5)
+        product_list.append(Product(name, price, weight, flammability))
     
     return product_list
+
+def inventory_report(li)
+    """Takes a list of producs and returns a summary."""
+    avg_price = mean([product.price for product in li])
+    avg_weight = mean([product.weight for product in li])
+    avg_flammability = mean([product.flammability for product in li])
+
+    print('ACME CORPORATION OFFICIAL INVENTORY REPORT')
+    print(f'Unique product names: {len(set(li))}')
+    print(f'Average price: {avg_price}')
+    print(f'Average weight: {avg_weight}')
+    print(f'Average flammability: {avg_flammability}')
