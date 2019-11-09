0a7de411dfd2148ee29c3a579fb50235a61eab0e
sapinspys <garri009@fiu.edu>
2019-11-08 19:56:29 -0800

    Used PEP8 to style all scripts

--- sprint-challenge/acme.py
+++ sprint-challenge/acme.py
@@ -1,6 +1,7 @@
 #!/usr/bin/env python
 from random import randint
 
+
 class Product:
     def __init__(self, name, price=10, weight=20, flammability=0.5):
         self.name = name
--- sprint-challenge/acme_report.py
+++ sprint-challenge/acme_report.py
@@ -6,18 +6,20 @@ from acme import Product
 adj = ['Awesome', 'Shiny', 'Impressive', 'Portable', 'Improved']
 noun = ['Anvil', 'Catapult' 'Disguise' 'Mousetrap', '???']
 
+
 def generate_products(count=30):
     """Takes in a number and generates a list of that number of products randomly generated from existing adjective and noun lists."""
     product_list = []
 
     for _ in range(count):
-        name =  f'{adj[randint(0,len(adj)-1)]} {noun[randint(0,len(noun)-1)]}'
-        price = randint(5,100)
+        name = f'{adj[randint(0,len(adj)-1)]} {noun[randint(0,len(noun)-1)]}'
+        price = randint(5, 100)
         weight = randint(5, 100)
         flammability = uniform(0, 2.5)
         product_list.append(Product(name, price, weight, flammability))
-    
+
     return product_list
+
 
 def inventory_report(li):
     """Takes a list of producs and returns a summary."""
